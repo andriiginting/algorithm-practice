ef297fceaeb013812071116dcf0f03093a6831da
andriiginting <andri.ginting@go-jek.com>
2022-05-05 18:12:51 +0700

    Update levels 3 problems

--- src/main/java/FoobarChallenge.java
+++ src/main/java/FoobarChallenge.java
@@ -9,7 +9,7 @@ public class FoobarChallenge {
         int[] y = new int[]{5, 2, 5, 13};
         int[] result = stationCodedMsg(x, 12);
 
-        int stairs = grandestStaircase(3);
+        int stairs = findAccessCode(new int[]{1, 2, 3, 4, 5, 6});
         System.out.println(stairs);
     }
 
@@ -284,7 +284,7 @@ Write a function called solution(n) that takes a positive integer n and returns 
 
     public static int grandestStaircase(int n) {
         Integer[][] stair = new Integer[n + 2][n + 2];
-        initStair(n+2, stair);
+        initStair(n + 2, stair);
         return staircaseHelper(1, n, stair) - 1;
     }
 
@@ -311,5 +311,53 @@ Write a function called solution(n) that takes a positive integer n and returns 
 
         stair[i][j] = staircaseHelper(i + 1, j - i, stair) + staircaseHelper(i + 1, j, stair);
         return stair[i][j];
+    }
+
+    /*
+    Find the Access Codes
+=====================
+
+In order to destroy Commander Lambda's LAMBCHOP doomsday device, you'll need access to it. But the only door leading to
+the LAMBCHOP chamber is secured with a unique lock system whose number of passcodes changes daily. Commander Lambda gets
+a report every day that includes the locks' access codes, but only the Commander knows how to figure out which of
+several
+ lists contains the access codes. You need to find a way to determine which list contains the access codes once you're
+ ready to go in.
+
+Fortunately, now that you're Commander Lambda's personal assistant, Lambda has confided to you that all the access codes
+ are "lucky triples" in order to make it easier to find them in the lists. A "lucky triple" is a tuple (x, y, z) where
+ x divides y and y divides z, such as (1, 2, 4). With that information, you can figure out which list contains the
+ number of access codes that matches the number of locks on the door when you're ready to go in (for example, if there's
+  5 passcodes, you'd need to find a list with 5 "lucky triple" access codes).
+
+Write a function solution(l) that takes a list of positive integers l and counts the number of "lucky triples"
+of (li, lj, lk) where the list indices meet the requirement i < j < k.  The length of l is between 2 and 2000 inclusive.
+  The elements of l are between 1 and 999999 inclusive.  The solution fits within a signed 32-bit integer. Some of the
+  lists are purposely generated without any access codes to throw off spies, so if no triples are found, return 0.
+
+For example, [1, 2, 3, 4, 5, 6] has the triples: [1, 2, 4], [1, 2, 6], [1, 3, 6], making the solution 3 total.
+     */
+
+    public static int findAccessCode(int[] l) {
+        int result = 0;
+        int[] arr = new int[l.length];
+
+        for (int i = 1; i < l.length - 1; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (l[i] % l[j] == 0) {
+                    ++arr[i];
+                }
+            }
+        }
+
+        for (int i = 2; i < l.length; i++) {
+            for (int j = 1; j < i; ++j) {
+                if (l[i] % l[j] == 0) {
+                    result += arr[j];
+                }
+            }
+        }
+
+        return result;
     }
 }
