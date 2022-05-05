6ab43362430551b9aede2517fd4cdedd8a73735e
andriiginting <andri.ginting@go-jek.com>
2022-05-05 17:27:25 +0700

    Update foobar level 3 problems

--- src/main/java/FoobarChallenge.java
+++ src/main/java/FoobarChallenge.java
@@ -1,7 +1,6 @@
 import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FoobarChallenge {
@@ -10,10 +9,8 @@ public class FoobarChallenge {
         int[] y = new int[]{5, 2, 5, 13};
         int[] result = stationCodedMsg(x, 12);
 
-        String[] msg = elevatorMaintenance(new String[]{"1.1.2", "1.0", "1.3.3", "1.0.12", "1.0.2"});
-        for (String value : msg) {
-            System.out.println(value);
-        }
+        int stairs = grandestStaircase(3);
+        System.out.println(stairs);
     }
 
     public static int solution(int[] x, int[] y) {
@@ -245,5 +242,74 @@ Output:
         };
         Arrays.sort(l, comparator);
         return l;
+    }
+
+    /*
+    The Grandest Staircase Of Them All
+==================================
+
+With the LAMBCHOP doomsday device finished, Commander Lambda is preparing to debut on the galactic stage -- but in order to make a grand entrance, Lambda needs a grand staircase! As the Commander's personal assistant, you've been tasked with figuring out how to build the best staircase EVER.
+
+Lambda has given you an overview of the types of bricks available, plus a budget. You can buy different amounts of the different types of bricks (for example, 3 little pink bricks, or 5 blue lace bricks). Commander Lambda wants to know how many different types of staircases can be built with each amount of bricks, so they can pick the one with the most options.
+
+Each type of staircase should consist of 2 or more steps.  No two steps are allowed to be at the same height - each step must be lower than the previous one. All steps must contain at least one brick. A step's height is classified as the total amount of bricks that make up that step.
+For example, when N = 3, you have only 1 choice of how to build the staircase, with the first step having a height of 2 and the second step having a height of 1: (# indicates a brick)
+
+#
+##
+21
+
+When N = 4, you still only have 1 staircase choice:
+
+#
+#
+##
+31
+
+But when N = 5, there are two ways you can build a staircase from the given bricks. The two staircases can have heights (4, 1) or (3, 2), as shown below:
+
+#
+#
+#
+##
+41
+
+#
+##
+##
+32
+
+Write a function called solution(n) that takes a positive integer n and returns the number of different staircases that can be built from exactly n bricks. n will always be at least 3 (so you can have a staircase at all), but no more than 200, because Commander Lambda's not made of money!
+     */
+
+    public static int grandestStaircase(int n) {
+        Integer[][] stair = new Integer[n + 2][n + 2];
+        initStair(n+2, stair);
+        return staircaseHelper(1, n, stair) - 1;
+    }
+
+    private static void initStair(int n, Integer[][] stair) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                stair[i][j] = 0;
+            }
+        }
+    }
+
+    private static int staircaseHelper(int i, int j, Integer[][] stair) {
+        if (stair[i][j] != 0) {
+            return stair[i][j];
+        }
+
+        if (j == 0) {
+            return 1;
+        }
+
+        if (j < i) {
+            return 0;
+        }
+
+        stair[i][j] = staircaseHelper(i + 1, j - i, stair) + staircaseHelper(i + 1, j, stair);
+        return stair[i][j];
     }
 }
